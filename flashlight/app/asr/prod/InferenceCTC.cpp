d95fe054b1a2fc60d2cd93a67305a9b10b35443f
abhinavdayal <abhinav.dayal@vishnu.edu.in>
2021-12-24 15:23:20 +0530

    add jsonbuilder

--- flashlight/app/asr/prod/InferenceCTC.cpp
+++ flashlight/app/asr/prod/InferenceCTC.cpp
@@ -26,16 +26,8 @@
 #include "flashlight/ext/common/Serializer.h"
 #include "flashlight/lib/text/decoder/LexiconDecoder.h"
 #include "flashlight/lib/text/decoder/lm/KenLM.h"
-
-// #include "flashlight/app/asr/runtime/runtime.h"
-// #include "flashlight/app/asr/tools/alignment/Utils.h"
-// #include "flashlight/fl/flashlight.h"
-// #include "flashlight/lib/common/System.h"
-// #include "flashlight/lib/text/dictionary/Defines.h"
-// #include "flashlight/lib/text/dictionary/Dictionary.h"
-
-
-
+#include "jsonbuilder/include/jsonbuilder/JsonBuilder.h"
+#include "jsonbuilder/include/jsonbuilder/JsonRenderer.h"
 
 //<======================================== Member functions ========================================>
 void printArray(af::array arr,std::string name, bool printdata){
@@ -143,6 +135,314 @@ void loadModel(
         << ". This type is not supported in the tutorial";
   }
 }
+
+class Inference {
+  public:
+    std::shared_ptr<fl::Module> network;
+    std::unordered_map<std::string, std::string> networkFlags;
+    std::shared_ptr<fl::app::asr::SequenceCriterion> criterion;
+    fl::lib::text::Dictionary tokenDict;
+    fl::lib::text::LexiconMap lexicon;
+    fl::lib::text::Dictionary wordDict;
+    std::shared_ptr<fl::lib::text::KenLM> lm;
+    fl::lib::text::LexiconDecoder *decoder;
+    fl::Dataset::DataTransformFunction inputTransform;
+    std::string transcript;
+    int unkWordIdx;
+
+    Inference(
+      std::shared_ptr<fl::Module>& network,
+      std::unordered_map<std::string, std::string>& networkFlags,
+      std::shared_ptr<fl::app::asr::SequenceCriterion>& criterion,
+      fl::lib::text::Dictionary& tokenDict,
+      fl::lib::text::LexiconMap& lexicon,
+      fl::lib::text::Dictionary& wordDict,
+      std::shared_ptr<fl::lib::text::KenLM>& lm,
+      fl::lib::text::LexiconDecoder *decoder,
+      fl::Dataset::DataTransformFunction& inputTransform,
+      std::string& transcript,
+      int unkWordIdx
+    ) {
+      this->network = network;
+      this->decoder = decoder;
+      this->networkFlags = networkFlags;
+      this->tokenDict = tokenDict;
+      this->lexicon = lexicon;
+      this->wordDict = wordDict;
+      this->lm = lm;
+      this->inputTransform = inputTransform;
+      this->criterion = criterion;
+      this->transcript = transcript;
+      this->unkWordIdx = unkWordIdx;
+    }
+
+    fl::Variable Forward(fl::app::asr::SoundInfo& audioInfo, std::vector<float>& audio) {
+      std::cout<< "audio Info Details: channels = "<<audioInfo.channels<<" samplerate = "<<audioInfo.samplerate<<" frames = "<<audioInfo.frames<<std::endl;
+      std::cout<< "audio size = "<<audio.size()<<"frames\n";
+      std::cout<<"audio duration = "<<audioInfo.frames/audioInfo.samplerate<<std::endl;
+      af::array input = this->inputTransform(
+          static_cast<void*>(audio.data()),
+          af::dim4(audioInfo.channels, audioInfo.frames),
+          af::dtype::f32);
+      printArray(input,"input array ", false);
+      auto inputLen = af::constant(input.dims(0), af::dim4(1));
+      printArray(inputLen, "inputLen array ", false);
+      auto rawEmission = fl::ext::forwardSequentialModuleWithPadMask(
+         fl::input(input), this->network, inputLen);
+      printArray(rawEmission.array(), " Raw Emission array ", true);
+
+      return rawEmission;
+    }
+
+    std::string  Decode(int nframes, int ntokens, std::vector<float>& emission) {
+      // actually decode is not needed for evaluation
+      const auto& result = this->decoder->decode(
+        emission.data(),
+        nframes /* time */,
+        ntokens /* ntokens */);
+
+
+      std::cout<<"Result size = "<<result.size()<<std::endl;
+      int i = 0;
+      std::cout<<"Result vector containns....."<<std::endl;
+      // PRINT RESULTS
+      for(auto f:result){
+        std::cout<<"For position "<<i<<std::endl;
+        std::cout<<"Score = "<<f.score<<" am score = "<<f.amScore<<" lmscore = "<<f.lmScore<<std::endl;
+        // std::cout<<"Words size = "<<f.words.size()<<" Words are ............\n";
+        // for(auto word:f.words){
+        //   std::cout<<word<<" ";
+        // }
+        std::cout<<std::endl<<"Tokens size = "<<f.tokens.size()<<" Tokens are..........."<<std::endl;
+        for(auto token:f.tokens){
+          if(tokenDict.getEntry(token)!="#")
+          std::cout<<tokenDict.getEntry(token)<<" ";
+        }
+        std::cout<<std::endl;
+        i++;
+        if(i==10)
+        break;
+      }
+      std::cout<<std::endl;
+      // Take top hypothesis and cleanup predictions
+      auto rawWordPrediction = result[0].words;
+      auto rawTokenPrediction = result[0].tokens;
+
+      auto tokenPrediction = fl::app::asr::tknPrediction2Ltr(
+          rawTokenPrediction,
+          this->tokenDict,
+          fl::app::asr::kCtcCriterion,
+          this->networkFlags["surround"],
+          false /* eostoken */,
+          0 /* replabel */,
+          false /* usewordpiece */,
+          this->networkFlags["wordseparator"]);
+      rawWordPrediction =
+          fl::app::asr::validateIdx(rawWordPrediction, this->unkWordIdx);
+      auto wordPrediction = fl::app::asr::wrdIdx2Wrd(rawWordPrediction, wordDict);
+      auto wordPredictionStr = fl::lib::join(" ", wordPrediction);
+
+      LOG(INFO) << "[Inference tutorial for CTC]: predicted output: "
+                << wordPredictionStr;
+
+      return wordPredictionStr;
+    }
+
+    af::array ForceAlign(fl::Variable& rawEmission) {
+      int i, j;
+      std::vector<int> targetvec(this->transcript.size()+1);
+      for(i=0; i<this->transcript.size(); i++) {
+        char ch = this->transcript.at(i);
+        if (ch==' ') ch = '|';
+        targetvec[i] = this->tokenDict.getIndex(std::string(1, ch));
+      }
+      targetvec[i] = this->tokenDict.getIndex("|");
+
+      auto target = af::array(targetvec.size(), 1, targetvec.data());
+      std::cout << "target Dims " << target.dims(0) << " X " << target.dims(1) << " X " << target.dims(2) << " X " << target.dims(3) << " X " << std::endl;
+      std::cout << "Now doing force alignment using output: " << this->transcript << std::endl;
+      // Now doing force alignment and printing the softmax values
+      // calculate softmax for rawEmission
+      auto paths = this->criterion->viterbiPathWithTarget(
+          rawEmission.array(), target);
+
+      return paths;
+    }
+
+    std::vector<std::vector<float>> GetSoftMaxProbs(int nframes, int ntokens, std::vector<float>& emission) {
+      std::cout << "Now calculating softmax from emissions" << std::endl;
+// calculate the softmax probabilities assuming column major order of emission vector.
+      std::vector<std::vector<float>> probs(ntokens, std::vector<float>(nframes, 0));
+      
+      int i, j;
+
+      for(i=0; i<ntokens; i++) {
+          std::cout << this->tokenDict.getEntry(i) << " ";
+      }
+      std::cout << std::endl;
+
+      for(j=0; j<nframes; j++) {
+        float s = 0;
+        for(i=0; i<ntokens; i++) {
+          probs[i][j] = exp(emission[j*ntokens+i]);
+          s += probs[i][j];
+        }
+        int mindex = 0;
+        for(i=0; i<ntokens; i++) {
+          probs[i][j] /= s;
+          if (probs[mindex][j] < probs[i][j]) mindex = i;
+        }
+        std::cout << "frame " << j << ": " << this->tokenDict.getEntry(mindex) << ":" << probs[mindex][j] << std::endl;
+      }
+
+      return probs;
+    }
+
+    const char* Evaluate(af::array paths, std::vector<std::vector<float>> probs, std::string transcript) {
+      // bestPaths contain the index tokens
+      // we need to return:
+      // word start and end times
+      // word score
+      // overall score
+      // return a JSON string from here.
+      jsonbuilder::JsonBuilder jb;
+      jsonbuilder::JsonIterator wordsIterator = jb.push_back(jb.end(), "words", jsonbuilder::JsonArray);
+      
+      const int B = paths.dims(1);
+      const int T = paths.dims(0);
+      std::cout << "Paths dims = " << B << "x" << T << std::endl;
+      float score = 0;
+      int len = 0;
+      std::vector<std::string> tokens;
+      bool inside_word = false;
+      std::string cur_word = "";
+      float word_score = 0;
+      int word_len = 0;
+      int word_start = 0;
+      int word_end = 0;
+
+      int t;
+
+      for (t = 0; t < T; t++) {
+        int p = paths(t, 0).scalar<int>();
+        if (p == -1) {
+          break;
+        }
+        auto token = tokenDict.getEntry(p);
+        tokens.push_back(token);
+        std::cout << t << ": " << token << std::endl;
+
+        // if a word haas started
+        if(!inside_word && p!=28 && p!=1) {
+          inside_word = true;
+          word_start = t;
+          word_score = 0;
+          word_len = 0;
+          cur_word = "";
+        }
+
+        // if a wrd has ended
+        if(inside_word && p==1) {
+          inside_word = false;
+          if (cur_word.back()=='#') {
+            cur_word.pop_back();
+          }
+          word_score /= word_len;
+          word_end = t-1;
+          // add to json the word and the word score
+          jsonbuilder::JsonIterator singlewordIterator = jb.push_back(wordsIterator, "", jsonbuilder::JsonObject);
+          jb.push_back(singlewordIterator, "word", cur_word);
+          jb.push_back(singlewordIterator, "start_time", word_start*30);
+          jb.push_back(singlewordIterator, "end_time", word_end*30);
+          jb.push_back(singlewordIterator, "score", word_score);
+          std::cout << cur_word << " "  << word_score << std::endl;
+        }
+
+        // otherwise inside a word
+        if(inside_word) {
+          if(cur_word.size()==0 || cur_word.back()!=token.at(0)) {
+            if (cur_word.back()=='#') {
+              cur_word.back() = token.at(0);
+            } else {
+              cur_word += token.at(0);
+            }
+            if(p!=28) {
+              word_score += probs[p][t];
+              word_len++;
+            }
+          }
+        }
+        if(p!=28 && p!=1) {
+          score += probs[p][t];
+          len++;
+        }
+      }
+
+      if(inside_word) {
+        inside_word = false;
+        if (cur_word.back()=='#') {
+          cur_word.pop_back();
+        }
+        word_score /= word_len;
+        word_end = t-1;
+        // add to json the word and the word score
+        jsonbuilder::JsonIterator singlewordIterator = jb.push_back(wordsIterator, "", jsonbuilder::JsonObject);
+        jb.push_back(singlewordIterator, "word", cur_word);
+        jb.push_back(singlewordIterator, "start_time", word_start*30);
+        jb.push_back(singlewordIterator, "end_time", word_end*30);
+        jb.push_back(singlewordIterator, "score", word_score);
+        std::cout << cur_word << " "  << word_score << std::endl;
+      }
+      jb.push_back(jb.end(), "score", score/len);
+      jb.push_back(jb.end(), "transcript", transcript);
+
+      std::cout << "SCORE: "  << score/len << std::endl;
+
+      jsonbuilder::JsonRenderer renderer;
+      renderer.Reserve(2048);
+
+      std::string_view result = renderer.Render(jb);
+      std::string stl_string(result.data(), result.size());
+
+      std::cout << stl_string.c_str() << std::endl;
+
+      return stl_string.c_str();
+    }
+
+
+    void run(fl::app::asr::SoundInfo& audioInfo, std::vector<float>& audio) {
+      // run forward pass
+      fl::Variable rawEmission = this->Forward(audioInfo, audio);
+
+      // prepare emission vector
+      auto emission = fl::ext::afToVector<float>(rawEmission);
+      std::cout<<"size of emission vector = "<<emission.size()<<std::endl;
+
+      // calculate softmax from emissions for scoring later
+      int nframes = rawEmission.dims(1); /* time */
+      int ntokens = rawEmission.dims(0);
+      auto probs = GetSoftMaxProbs(nframes, ntokens, emission);
+
+      // decode the best outcome
+      std::string decoded_text = Decode(nframes, ntokens, emission);
+
+      // NOTE: if this>transcript is not there use decoded_text as transcript instead.
+
+      // force align the intended transcript, if nothing than use current one
+      auto alignedpath = ForceAlign(rawEmission);
+      
+      // evaluate the score and determing word boundaries
+      auto jsonResponse = Evaluate(alignedpath, probs, decoded_text);
+
+      std::cout << jsonResponse << std::endl;
+
+      // add other info to the json response and finally return
+      // return result
+
+      // NOTE:- the actual words are word groups, so we may have to do some post alignment based upon speech hints as well
+      // In the demo we will not do that though.
+  }
+};
 
 
 int main(int argc, char** argv) {
@@ -264,14 +564,21 @@ int main(int argc, char** argv) {
        networkFlags["localnrmlrightctx"] == "true"},
       /*sfxConf=*/{});
   fl::EditDistanceMeter dst;
+
   /* ===================== Inference ===================== */
   bool interactive = FLAGS_audio_list == "";
   std::ifstream audioListStream;
   if (!interactive) {
     audioListStream = std::ifstream(FLAGS_audio_list);
   }
+
+  std::string transcript = "hello world";
+  // create an inference engine
+  Inference inferEngine(network,networkFlags,criterion,tokenDict,lexicon,wordDict,lm,&decoder,inputTransform,transcript, unkWordIdx);
+  
   while (true) {
     std::string audioPath;
+    // need to work out a socket here
     if (interactive) {
       LOG(INFO)
           << "[Inference tutorial for CTC]: Waiting the input in the format [audio_path].";
@@ -294,163 +601,8 @@ int main(int argc, char** argv) {
     auto audioInfo = fl::app::asr::loadSoundInfo(audioPath.c_str());
     auto audio = fl::app::asr::loadSound<float>(audioPath.c_str());
     std::cout<< "While doing inference for " << audioPath << " file\n";
-    std::cout<< "audio Info Details: channels = "<<audioInfo.channels<<" samplerate = "<<audioInfo.samplerate<<" frames = "<<audioInfo.frames<<std::endl;
-    std::cout<< "audio size = "<<audio.size()<<"frames\n";
-    std::cout<<"audio duration = "<<audioInfo.frames/audioInfo.samplerate<<std::endl;
-    af::array input = inputTransform(
-        static_cast<void*>(audio.data()),
-        af::dim4(audioInfo.channels, audioInfo.frames),
-        af::dtype::f32);
-    printArray(input,"input array ", false);
-    auto inputLen = af::constant(input.dims(0), af::dim4(1));
-    printArray(inputLen, "inputLen array ", false);
-    auto rawEmission = fl::ext::forwardSequentialModuleWithPadMask(
-        fl::input(input), network, inputLen);
-    printArray(rawEmission.array(), " Raw Emission array ", true);
-    auto emission = fl::ext::afToVector<float>(rawEmission);
-    std::cout<<"size of emission vector = "<<emission.size()<<std::endl;
-    int i=0, j;
-    for(auto f:emission){
-      std::cout<<f<<" ";
-      i++;
-      if(i==10)
-      break;
-    }
-    std::cout<<std::endl;
-    const auto& result = decoder.decode(
-        emission.data(),
-        rawEmission.dims(1) /* time */,
-        rawEmission.dims(0) /* ntokens */);
-
-
-    std::cout<<"Result size = "<<result.size()<<std::endl;
-    i = 0;
-    std::cout<<"Result vector containns....."<<std::endl;
-    for(auto f:result){
-      std::cout<<"For position "<<i<<std::endl;
-      std::cout<<"Score = "<<f.score<<" am score = "<<f.amScore<<" lmscore = "<<f.lmScore<<std::endl;
-      // std::cout<<"Words size = "<<f.words.size()<<" Words are ............\n";
-      // for(auto word:f.words){
-      //   std::cout<<word<<" ";
-      // }
-      std::cout<<std::endl<<"Tokens size = "<<f.tokens.size()<<" Tokens are..........."<<std::endl;
-      for(auto token:f.tokens){
-        if(tokenDict.getEntry(token)!="#")
-        std::cout<<tokenDict.getEntry(token)<<" ";
-      }
-      std::cout<<std::endl;
-      i++;
-      if(i==10)
-      break;
-    }
-    std::cout<<std::endl;
-    // Take top hypothesis and cleanup predictions
-    auto rawWordPrediction = result[0].words;
-    auto rawTokenPrediction = result[0].tokens;
-
-    // ************* Target generation and Alignment******************
-
-    // fl::app::asr::TargetGenerationConfig targetGenConfig(
-    //   FLAGS_wordseparator, //std::atoll(networkFlags["framesizems"].c_str()),
-    //   FLAGS_sampletarget,
-    //   FLAGS_criterion,
-    //   FLAGS_surround,
-    //   isSeq2seqCrit,
-    //   FLAGS_replabel,
-    //   true /* skip unk */,
-    //   FLAGS_usewordpiece /* fallback2LetterWordSepLeft */,
-    //   !FLAGS_usewordpiece /* fallback2LetterWordSepLeft */);
-
-    // auto targetTransform = fl::app::asr::targetFeatures(tokenDict, lexicon, targetGenConfig);
-    // auto wordTransform = fl::app::asr::wordFeatures(wordDict);
-
+    inferEngine.run(audioInfo, audio);
     
-
-    auto tokenPrediction = fl::app::asr::tknPrediction2Ltr(
-        rawTokenPrediction,
-        tokenDict,
-        fl::app::asr::kCtcCriterion,
-        networkFlags["surround"],
-        false /* eostoken */,
-        0 /* replabel */,
-        false /* usewordpiece */,
-        networkFlags["wordseparator"]);
-    rawWordPrediction =
-        fl::app::asr::validateIdx(rawWordPrediction, unkWordIdx);
-    auto wordPrediction = fl::app::asr::wrdIdx2Wrd(rawWordPrediction, wordDict);
-    auto wordPredictionStr = fl::lib::join(" ", wordPrediction);
-
-    
-    std::string hardcodedoutput = "hello world";//"this is a test";
-    std::vector<int> targetvec(hardcodedoutput.size()+1);
-    for(i=0; i<hardcodedoutput.size(); i++) {
-      char ch = hardcodedoutput.at(i);
-      if (ch==' ') ch = '|';
-      targetvec[i] = tokenDict.getIndex(std::string(1, ch));
-    }
-    targetvec[i] = tokenDict.getIndex("|");
-
-    auto target = af::array(targetvec.size(), 1, targetvec.data());
-    std::cout << "target Dims " << target.dims(0) << " X " << target.dims(1) << " X " << target.dims(2) << " X " << target.dims(3) << " X " << std::endl;
-
-    
-    LOG(INFO) << "[Inference tutorial for CTC]: predicted output for "
-              << audioPath << "\n"
-              << wordPredictionStr;
-
-    std::cout << "Now doing force alignment using output: " << wordPredictionStr << std::endl;
-    // Now doing force alignment and printing the softmax values
-    // calculate softmax for rawEmission
-    auto paths = criterion->viterbiPathWithTarget(
-        rawEmission.array(), target);
-
-    std::cout << "Now calculating softmax from emissions" << std::endl;
-    // calculate the softmax probabilities assuming column major order of emission vector.
-    int nframes = rawEmission.dims(1); /* time */
-    int ntokens = rawEmission.dims(0);
-
-    std::vector<std::vector<float>> probs(ntokens, std::vector<float>(nframes, 0));
-    
-    for(i=0; i<ntokens; i++) {
-        std::cout << tokenDict.getEntry(i) << " ";
-    }
-    std::cout << std::endl;
-
-    for(j=0; j<nframes; j++) {
-      float s = 0;
-      for(i=0; i<ntokens; i++) {
-        probs[i][j] = exp(emission[j*ntokens+i]);
-        s += probs[i][j];
-      }
-      int mindex = 0;
-      for(i=0; i<ntokens; i++) {
-        probs[i][j] /= s;
-        if (probs[mindex][j] < probs[i][j]) mindex = i;
-      }
-      std::cout << "frame " << j << ": " << tokenDict.getEntry(mindex) << ":" << probs[mindex][j] << std::endl;
-    }
-
-    // bestPaths contain the index tokens
-    const int B = paths.dims(1);
-    const int T = paths.dims(0);
-    std::cout << "Paths dims = " << B << "x" << T << std::endl;
-    std::vector<std::vector<std::string>> batchTokensPath;
-    float score = 0;
-    for (int b = 0; b < B; b++) {
-      std::vector<std::string> tokens;
-      for (int t = 0; t < T; t++) {
-        int p = paths(t, b).scalar<int>();
-        if (p == -1) {
-          break;
-        }
-        auto token = tokenDict.getEntry(p);
-        tokens.push_back(token);
-        std::cout << t << ": " << token << std::endl;
-        if(p!=28 && p!=1) score += probs[p][t];
-      }
-      std::cout << "SCORE = " << score << std::endl;
-      batchTokensPath.push_back(tokens);
-    }
   }
   return 0;
 }
